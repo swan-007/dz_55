416b5290dbd896a3416cede4712c3451fd65ced0
Sergey Lebedev <superlebedew@gmail.com>
2023-05-04 13:07:38 +0300

    got

---
+++ api_with_restrictions/advertisements/serializers.py
@@ -0,0 +1,40 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+from .models import Advertisement
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """Serializer для пользователя."""
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'first_name',
+                  'last_name',)
+
+
+class AdvertisementSerializer(serializers.ModelSerializer):
+    """Serializer для объявления."""
+
+    creator = UserSerializer(
+        read_only=True,
+    )
+
+    class Meta:
+        model = Advertisement
+        fields = ('id', 'title', 'description', 'creator',
+                  'status', 'created_at',)
+
+    def create(self, validated_data):
+        """Метод для создания"""
+        validated_data["creator"] = self.context["request"].user
+        return super().create(validated_data)
+
+    def validate(self, data):
+        """Метод для валидации. Вызывается при создании и обновлении."""
+        user = self.context['request'].user
+        if not self.instance:
+            if Advertisement.objects.filter(creator=user).count() > 10:
+                raise serializers.ValidationError(f'User {user} can\'t create more then 10 items!')
+
+        return data
---
+++ api_with_restrictions/advertisements/views.py
@@ -0,0 +1,23 @@
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
+from .models import Advertisement
+from .serializers import AdvertisementSerializer
+from django_filters import rest_framework as filters
+from .filters import AdvertisementFilter
+from .permissions import IsAdvertisementOwner
+
+
+class AdvertisementViewSet(ModelViewSet):
+    """ViewSet для объявлений."""
+    queryset = Advertisement.objects.all()
+    serializer_class = AdvertisementSerializer
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_class = AdvertisementFilter
+
+    def get_permissions(self):
+        """Получение прав для действий."""
+        if self.action == "create":
+            return [IsAuthenticated()]
+        elif self.action in ["update", "partial_update", "delete"]:
+            return [IsAuthenticated(), IsAdvertisementOwner()]
+        return []
